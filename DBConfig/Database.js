4c7adda24e3ca7ad7da26f3a884d2de1e0f0aacd
mathanNeoStark99 <120645133+mathanNeoStark99@users.noreply.github.com>
2022-12-23 13:32:08 +0530

    Update Database.js

--- DBConfig/Database.js
+++ DBConfig/Database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "examly",
-    DB: "books",
+    HOST: "sql6.freemysqlhosting.net",
+    USER: "sql6586206",
+    PASSWORD: "Z3SkPldcea",
+    DB: "sql6586206",
     dialect: "mysql"
   };
